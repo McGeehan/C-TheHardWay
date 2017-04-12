0466b457601f48cc45a9592498877827d10f4fca
Matt <mmcgeehan@paduaacademy.org>
2017-04-12 10:46:21 -0400

    Add files via upload

---
+++ first.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Test
+{
+     public static void Main()
+     {
+          Console.WriteLine("Hello World!");
+          Console.WriteLine("Hello Again");
+          Console.WriteLine("I like typing this.");
+          Console.WriteLine("This is fun.");
+          Console.WriteLine("I'd much rather you 'not'.");
+     }
+}
---
+++ second.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Test
+{
+  public static void Main()
+  	{
+  // A comment, this is so you can read your program later.
+  //Anything after the // is ignored by C#.
+  Console.WriteLine("I could have code like this."); //and the comment is ignored after
+
+  //Console.WriteLine("This won't run.");
+  Console.WriteLine("This will run.");
+  	}
+}
